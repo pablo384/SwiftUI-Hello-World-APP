c82d1c8dee0d9f575311993f946668ba35628ba0
Pablo384 <pabloreinoso0@gmail.com>
2019-09-25 21:44:50 -0400

    pROBANDO COSAS NUEVAS

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -7,30 +7,83 @@
 //
 
 import SwiftUI
+var txt: String = "";
 
 struct ContentView: View {
+    @State private var name: String = "Tim"
+    @State private var che: Bool = false
+    
     var body: some View {
         VStack {
-            Text("Hello World")
-                .font(.headline)
-                .fontWeight(.bold)
-                .foregroundColor(.blue)
-            HStack{
-                
-                Text("Hello World")
-                    .font(.body)
-                    .fontWeight(.bold)
-                    .foregroundColor(.blue)
-                Spacer()
-                Text("This is a nice caption")
-                    .font(.caption)
-                    .fontWeight(.bold)
-                    .foregroundColor(.red)
-            }
-            .padding(.all)
-            .padding(.init(30))
+            TextField("Enter your name", text: $name )
+            Toggle(isOn: $che, label: {
+                Text("cheche:asdfasdfasf:")
+            })
+            Text("Hello, \(name)!")
         }
-        
+    }
+}
+
+//struct ContentView: View {
+//    var body: some View {
+//        ScrollView(.vertical) {
+//            VStack {
+//                Button(
+//
+//                    action: {
+//                        print("CLICK asdf adf asd")
+//                    },
+//                    label: {
+//                        Text("Hello World")
+//                            .foregroundColor(Color.init(UIColor.red))
+//                    }
+//                )
+//
+//
+//
+//                Image("img").resizable()
+//                .frame(width: 32.0, height: 32.0)
+//
+////                MyTextField()
+//
+//                Text("Hello World")
+//                    .font(.headline)
+//                    .fontWeight(.bold)
+//                    .foregroundColor(.blue)
+//                HStack{
+//
+//                    Text("Hello World")
+//                        .font(.body)
+//                        .fontWeight(.bold)
+//                        .foregroundColor(.blue)
+//                    Spacer()
+//                    Text("This is a nice caption")
+//                        .font(.caption)
+//                        .fontWeight(.bold)
+//                        .foregroundColor(.red)
+//                }
+//                .padding(.all)
+//                .padding(.init(30))
+//            }
+//        }
+//
+//
+//    }
+//}
+struct MyTextField: View {
+    @Binding var text: String
+    var label: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(label)
+                Spacer()
+            }
+            TextField("", text: $text) //here we have a simple TextField
+            Divider()
+        }
+        .padding()
     }
 }
 
